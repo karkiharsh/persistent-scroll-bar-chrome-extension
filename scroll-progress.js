90e92ba351a526b9024798bacf0d5ad7b75c1d39
Harsh Karki <corecorecrorecrore@gmail.com>
2024-07-22 18:52:07 +0100

    add logic for scroll-bar

---
+++ scroll-progress.js
@@ -0,0 +1,16 @@
+export default function calculateScrollProgress() {
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.documentElement.scrollHeight;
+  const scrollTop =
+    window.ScrollY ||
+    window.pageYOffset ||
+    document.body.scrollTop +
+      ((document.documentElement && document.documentElemetn.scrollTop) || 0);
+
+  const scrolled = scrollTop + windowHeight;
+  const progress = (scrolled / documentHeight) * 100;
+
+  return progress.toFixed(2);
+}
+
+//add event Listener for scroll
