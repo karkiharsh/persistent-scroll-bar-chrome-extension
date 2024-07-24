9055e28c70c09439c5b6445c70191b24ac299671
Harsh Karki <corecorecrorecrore@gmail.com>
2024-07-24 19:05:07 +0100

    check consistency of scroll related properties on chrome

--- scroll-progress.js
+++ scroll-progress.js
@@ -1,6 +1,24 @@
+//browser behaviour keeps on changing , so for consistency we must check our program
+// across different browsers!
+
 export default function calculateScrollProgress() {
   const windowHeight = window.innerHeight;
   const documentHeight = document.documentElement.scrollHeight;
+
+  // window.ScrollY -> read only
+
+  // document.body.scrollTop -> pretty consistent to set value
+
+  /*  
+  window.scrollY and document.body.scrollTop are related and consistent on chrome . 
+
+  
+      console.log('Current scroll position:');
+      console.log('window.scrollY:', window.scrollY); 
+      console.log('document.documentElement.scrollTop:', document.documentElement.scrollTop);
+      console.log('document.body.scrollTop:', document.body.scrollTop);
+*/
+
   const scrollTop =
     window.ScrollY ||
     window.pageYOffset ||
@@ -13,6 +31,15 @@ export default function calculateScrollProgress() {
   return progress.toFixed(2);
 }
 
+//manually set scroll bar through :
+
+/*
+window.scrollTo(0,y)
+window.scrollBy(x,y) -> scrolls by amount
+
+
+
+*/
 //add event Listener for scroll
 
 document.addEventListener("scroll", () => {
