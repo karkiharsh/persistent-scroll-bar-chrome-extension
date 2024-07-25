f6485af0533a57d8aec299c38ef1d3ea9d645a00
Harsh Karki <corecorecrorecrore@gmail.com>
2024-07-25 18:21:06 +0100

    add comments for a refined design

--- fragment.js
+++ fragment.js
@@ -1,1 +1,7 @@
 // look for the node .. and highlight it  from the beginning with a fading look
+
+/* 
+
+
+
+*/
--- local-storage.js
+++ local-storage.js
@@ -1,6 +1,24 @@
+//creating a cookie based on href
+
+//calling this function every time user scrolls
+
+export function save_website_scroll_state() {
+  var website_href = window.location.search;
+}
+
 export default function saveSiteScrollData(website, scrollTop) {
   //website -> string
   // scrollTop -> position of scroll bar
 
   localStorage.setItem(website, scrollTop);
 }
+
+/* Issues 
+
+
+what about sites like youtube,facebook ?
+
+
+
+
+*/
