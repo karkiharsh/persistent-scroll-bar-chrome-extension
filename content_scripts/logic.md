37ddb419e43b04072e3844a83f658081507e03ec
Harsh Karki <corecorecrorecrore@gmail.com>
2024-07-28 15:27:47 +0100

    add basic logic ..wasted a hell lot of time

---
+++ content_scripts/logic.md
@@ -0,0 +1,21 @@
+- Step 0:
+  Where to actually place the code .
+  Configuration for manifest.json .
+
+- Step 1:
+  Save scroll bar position .
+  Save to local storage .
+
+Still got time ?
+
+- Step 2:
+  On selecting any element.
+  It will get a fading highlight which would basically mean that this is where you left.
+  And cookie value will change to make this selected text appear at center .
+
+Still got time ?
+
+- Step 3:
+
+  If user sign-up or log-in
+  he could use tihs scrolling data in any of his account .
--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert("unpacked extension loaded");
+// alert("unpacked extension loaded");
+alert("hi");
+
+console.log(document);
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log(entry.target, "is in the viewport");
+      }
+    });
+  },
+  {
+    root: null, // Use the viewport as the root
+    rootMargin: "0px",
+    threshold: 0.1, // Adjust this threshold to control when the callback is triggered
+  }
+);
+
+// Observe all elements
+
+// wrap this function in an event listener
+
+document.querySelectorAll("*").forEach((el) => {
+  observer.observe(el);
+});
